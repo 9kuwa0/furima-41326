d6b66daa72259c945e2ad9de85f4489285879171
9kuwa0 <techkuwa@gmail.com>
2024-10-02 21:56:27 +0900

    userモデルの単体テストコード記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
     validates :last_name_kanji
   end
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
-    validates :first_name_kana,
+    validates :first_name_kana
     validates :last_name_kana
   end
 
