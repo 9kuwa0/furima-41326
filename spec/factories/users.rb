6b6768b8832fd0b6db7fcc9206e9e05be9f7eca5
9kuwa0 <techkuwa@gmail.com>
2024-10-04 14:29:38 +0900

    rubocop後の修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials }
     email                 { Faker::Internet.email }
-    password              { "a1 #{Faker::Internet.password(min_length: 6)}" }
+    password              { "a1#{Faker::Internet.password(min_length: 6)}" }
     password_confirmation { password }
     last_name_kanji       { '山田' }
     first_name_kanji      { '太郎' }
