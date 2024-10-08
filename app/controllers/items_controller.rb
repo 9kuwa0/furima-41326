1b313f54b32e4fe4c835368487ae2be8176eebcf
9kuwa0 <techkuwa@gmail.com>
2024-10-08 16:35:01 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,25 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :item_price).merge(user_id: current_user.id)
+  end
+
 end
