69fe99708136ee577f3dd74b646411d711b3ca4a
9kuwa0 <techkuwa@gmail.com>
2024-10-10 22:06:42 +0900

    モデルとコントローラー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :record
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :Condition
+  belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_day
@@ -18,5 +19,5 @@ class Item < ApplicationRecord
   validates :prefecture_id,     presence: true, numericality: {other_than: 1, message: "can't be blank"}
   validates :shipping_day_id,   presence: true, numericality: {other_than: 1, message: "can't be blank"}
   validates :item_price,        presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-
+  validates :image,             presence: true
 end
