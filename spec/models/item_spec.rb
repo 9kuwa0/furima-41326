ffcd395f8c89009baff25e284048f51cd2e4e29f
9kuwa0 <techkuwa@gmail.com>
2024-10-10 22:07:30 +0900

    item単体テスト記入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,32 +9,66 @@ RSpec.describe Item, type: :model do
     # 正常系
     context '商品の出品ができる場合' do
       it '全ての項目があれば出品できる' do
+        expect(@item).to be_valid
       end
     end
 
     #異常系
     context '商品の出品ができない場合' do
       it '画像がないと出品ができない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it '商品の名前が空だと出品ができない' do
+        @item.item_name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
       it '商品の説明が空だと出品ができない' do
+        @item.item_description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description can't be blank")
       end
       it '商品のカテゴリーが1のｰｰｰだと出品ができない' do
+        @item.item_category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item category can't be blank")
       end
       it '送料が1の---だと出品ができない' do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
       it '発送元が1の---だと出品ができない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '商品の到着日数が1の---だと出品ができない' do
+        @item.shipping_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
       it '価格が未入力だと出品ができない' do
+        @item.item_price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price can't be blank")
       end
       it '価格が300円未満だと出品ができない' do
+        @item.item_price = 10
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price must be greater than or equal to 300")
       end
       it '価格が9999999円より多いと出品ができない' do
+        @item.item_price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price must be less than or equal to 9999999")
       end
       it '価格に小数点があると出品できない' do
+        @item.item_price = 1000.6
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price must be an integer")
       end
     end
   end
