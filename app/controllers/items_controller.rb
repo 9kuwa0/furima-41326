97ae925914a324bcc5028ea9127b1581534b1ace
9kuwa0 <techkuwa@gmail.com>
2024-10-12 20:08:50 +0900

    入力に問題があると入力してある情報は消えずエディットページに戻る

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,6 +32,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
+      render :edit, status: :unprocessable_entity
     end
   end
   
