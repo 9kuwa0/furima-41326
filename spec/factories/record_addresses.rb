fd17be2f7a98b7f1f4669818ebec6139e76cb6d0
9kuwa0 <techkuwa@gmail.com>
2024-10-25 22:25:46 +0900

    record_addressのファクトリーボットを作成

---
+++ spec/factories/record_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :donation_address do
+    token          { "tok_abcdefghijk00000000000000000" }
+    post_code      { "123-4567" }
+    prefecture_id  { Faker::Number.between(from: 2, to: 48)}
+    municipality   { "岐阜市" }
+    block          { "金華山天守閣18" }
+    building       { "岐阜城" }
+    phone_num      { "0120999999" }
+  end
+end
