e7e7e44c53ac1fada61e5bbe880bacec487a24fc
9kuwa0 <techkuwa@gmail.com>
2024-10-26 22:37:45 +0900

    修正３

--- spec/models/record_address_spec.rb
+++ spec/models/record_address_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe RecordAddress, type: :model do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:test_item, user_id: @user.id)
-    @another_user = FactoryBot.create(:user)
-    @record_address = FactoryBot.build(:record_address, user_id: @another_user.id, item_id: @item.id)
+    @record_address = FactoryBot.build(:record_address, user_id: @user.id, item_id: @item.id)
   end
 
   describe '商品購入' do
